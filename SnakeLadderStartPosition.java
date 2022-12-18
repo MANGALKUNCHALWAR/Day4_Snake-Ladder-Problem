7144a754e9ec055da7ad48dff221fcaf4103242c
John Doe <mangalkunchalwar9999@gmail.com>
2022-12-18 17:45:25 +0530

    Snake & Ladder Start Position zero.

---
+++ SnakeLadderStartPosition.java
@@ -0,0 +1,11 @@
+package assignment4.snakeandladderproblem;
+
+//1)Snake and Ladder game played with single player at start position 0
+
+public class SnakeLadderStartPosition {
+    public static void main(String[] args) {
+        int START_POSITION= 0;
+        System.out.println("Start Position : " + START_POSITION);
+    }
+}
+
